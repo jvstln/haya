856be4d3663cc72cdb441856e1ce1773366574f5
Justin Ezema <justin3z3m4@gmail.com>
2026-05-05 16:47:18 +0100

    General improvements on monorepo dx

--- apps/landing/components/footer.tsx
+++ apps/landing/components/footer.tsx
@@ -71,7 +71,7 @@ export function CTA() {
           />
           <InputGroupAddon align="inline-end">
             <Button>
-              Request access
+              Run free audit
               <ArrowRight />
             </Button>
           </InputGroupAddon>
